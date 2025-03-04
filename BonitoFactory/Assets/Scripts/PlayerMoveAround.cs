06b7a6943dc31f8adf3f400a2774307346193556
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2025-03-04 12:22:57 -0500

    12:22pm

--- BonitoFactory/Assets/Scripts/PlayerMoveAround.cs
+++ BonitoFactory/Assets/Scripts/PlayerMoveAround.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMoveAround : MonoBehaviour
 {
     public Transform cam;
     public float speed = 6f;
